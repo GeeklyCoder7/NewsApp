646e358c28d6f7a8c513ac930fbf6231e3285e16
Afwan Husaini <afwanhusaini07@gmail.com>
2024-09-20 12:16:59 +0530

    Checkpoint

--- app/src/main/java/com/example/newsapp/adapters/SavedArticlesRecyclerViewAdapter.java
+++ app/src/main/java/com/example/newsapp/adapters/SavedArticlesRecyclerViewAdapter.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.newsapp.R;
+import com.example.newsapp.databases.DatabaseHelper;
 import com.example.newsapp.databinding.ActivitySavedArticlesBinding;
 import com.example.newsapp.databinding.SavedArticlesSampleBinding;
 import com.example.newsapp.entities.ArticleEntity;
@@ -19,10 +20,12 @@ import java.util.ArrayList;
 public class SavedArticlesRecyclerViewAdapter extends RecyclerView.Adapter<SavedArticlesRecyclerViewAdapter.SavedArticlesRecyclerViewAdapterViewHolder> {
     Context context;
     ArrayList<ArticleEntity> articleEntityArrayList;
+    DatabaseHelper databaseHelper;
 
     public SavedArticlesRecyclerViewAdapter(Context context, ArrayList<ArticleEntity> articleEntityArrayList) {
         this.context = context;
         this.articleEntityArrayList = articleEntityArrayList;
+        databaseHelper = DatabaseHelper.getDatabase(context);
     }
 
     @NonNull
@@ -37,6 +40,13 @@ public class SavedArticlesRecyclerViewAdapter extends RecyclerView.Adapter<Saved
         holder.binding.newsHeadlineTextView.setText(articleEntity.getTitle());
         Glide.with(context).load(articleEntity.getUrlToImage()).into(holder.binding.newsImageView);
         holder.binding.newsDescriptionTextView.setText(articleEntity.getDescription());
+        holder.binding.removeArticleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHelper.articleDao().deleteArticle(articleEntity);
+                notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
